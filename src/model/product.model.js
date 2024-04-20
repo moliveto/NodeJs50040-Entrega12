38b736f9cb548d582eb85553b7a279487cce79e2
Marcelo Adrian Oliveto <marcelo.oliveto@gmail.com>
2024-04-20 16:55:37 -0300

    changes

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
-const collection = "Productos";
+const collection = "products";
 
 const schema = new mongoose.Schema({
-  name: String,
-  description: String,
-  quantity: Number,
-  price: Number,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  thumbnail: { type: String, default: 'Sin imagen' },
+  code: { type: String, required: true, unique: true, index: true },
+  quantity: { type: Number, required: true },
+  status: { type: Boolean, default: true },
 });
 
 schema.plugin(mongoosePaginate);
